e1efbd5863e9039ca6d6ab09d80ea2ed9440da26
Eric <eric_shih@sunion.com.tw>
2022-04-11 10:08:27 +0800

    Box-Button and toast

--- app/src/main/java/com/sunion/jetpacklock/MainActivity.kt
+++ app/src/main/java/com/sunion/jetpacklock/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.sunion.jetpacklock
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -20,12 +27,23 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun SimpleComposable() {
+    val mContext = LocalContext.current
     if (LocalInspectionMode.current) {
         // Show this text in a preview window
         Text("Hello preview user!")
     } else {
         // Show this text in the app:
-        Text("Hello World!")
+        Box(Modifier.fillMaxSize(), Alignment.Center) {
+            Button(
+                // ...
+//            onClick = myClickFunction
+                onClick = {
+                    Toast.makeText(mContext, "This is a Sample Toast", Toast.LENGTH_SHORT).show()
+                }
+            ){
+                Text("Button")
+            }
+        }
     }
 }
 
