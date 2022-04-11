62afb1dcd2e021892b45eb5c74d83d6614272ba6
Eric <eric_shih@sunion.com.tw>
2022-04-11 15:46:20 +0800

    Using AWS SDK Mobile-Client to SignIn/Out

--- app/src/main/java/com/sunion/jetpacklock/MainActivity.kt
+++ app/src/main/java/com/sunion/jetpacklock/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.sunion.jetpacklock
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.SignOutOptions
+import com.amazonaws.mobile.client.UserStateDetails
+import com.amazonaws.mobile.client.results.SignInResult
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Text("Hello World!")
             SimpleComposable()
         }
     }
@@ -27,23 +38,159 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun SimpleComposable() {
-    val mContext = LocalContext.current
-    if (LocalInspectionMode.current) {
-        // Show this text in a preview window
-        Text("Hello preview user!")
-    } else {
-        // Show this text in the app:
-        Box(Modifier.fillMaxSize(), Alignment.Center) {
+    val mContext = LocalContext.current // for Toast
+    //If init awsMobileClient here will take too many sources to display the preview correctly.
+    val awsMobileClient = AWSMobileClient.getInstance().apply {
+        initialize(mContext, object : Callback<UserStateDetails> {
+            override fun onResult(result: UserStateDetails?) {
+                Log.d("TAG",result.toString())
+            }
+
+            override fun onError(e: Exception?) {
+                Log.e("TAG",e.toString())
+            }
+        })
+    }
+    val idValue = remember { mutableStateOf(TextFieldValue()) }
+    val passwordValue = remember { mutableStateOf(TextFieldValue()) }
+    val openDialog = remember { mutableStateOf(false) }
+    val openLogoutDialog = remember { mutableStateOf(false) }
+    val logcat = remember { mutableStateOf("Welcome~\n\n") }
+
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
             Button(
-                // ...
-//            onClick = myClickFunction
                 onClick = {
-                    Toast.makeText(mContext, "This is a Sample Toast", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(mContext, "${idValue.value.text} ${passwordValue.value.text}", Toast.LENGTH_SHORT).show()
+                    openDialog.value = true
                 }
             ){
-                Text("Button")
+                Text("Login")
+            }
+            Button(
+                onClick = {
+                    openLogoutDialog.value = true
+                }
+            ){
+                Text("Logout")
+            }
+
+            Button(
+                onClick = {
+                    logcat.value = ""
+                }
+            ){
+                Text("Clean")
             }
         }
+        Text(text = logcat.value)
+    }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            title = {
+                Text(text = "Enter id and pw")
+            },
+            text = {
+                Column() {
+                    TextField(
+                        value = idValue.value,
+                        onValueChange = { idValue.value = it },
+                        placeholder = { Text(text = "Enter your id") },
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.None,
+                            autoCorrect = true,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                    TextField(
+                        value = passwordValue.value,
+                        onValueChange = { passwordValue.value = it },
+                        placeholder = { Text(text = "Enter your password") },
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.None,
+                            autoCorrect = true,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        awsMobileClient.signIn(idValue.value.text, passwordValue.value.text, null, object: Callback<SignInResult>{
+                            override fun onResult(result: SignInResult?) {
+                                logcat.value = logcat.value + "login status: ${result?.signInState}\n"
+                            }
+
+                            override fun onError(e: java.lang.Exception?) {
+                                logcat.value = logcat.value + "login error: $e\n"
+                            }
+
+                        })
+                        Toast.makeText(mContext, "${idValue.value.text} ${passwordValue.value.text}", Toast.LENGTH_SHORT).show()
+                        openDialog.value = false
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { openDialog.value = false }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
+    if (openLogoutDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                openLogoutDialog.value = false
+            },
+            title = {
+                Text(text = "Are you sure to log out?")
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        //SignOutOptions can not be null
+                        awsMobileClient.signOut(SignOutOptions.builder().build(), object :Callback<Void> {
+                            override fun onResult(result: Void?) {
+                                logcat.value = logcat.value + "logout success\n"
+                            }
+
+                            override fun onError(e: java.lang.Exception?) {
+                                logcat.value = logcat.value + "logout error: $e\n"
+                            }
+
+                        })
+                        openLogoutDialog.value = false
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { openLogoutDialog.value = false }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
     }
 }
 
